4518007e00df2d357e04a563708063eff13a8bc5
Jacob Price <j@cobprice.com>
2017-12-08 07:39:03 -0500

    Change file naming

--- cli/createPost.js
+++ cli/createPost.js
@@ -1,26 +1,29 @@
 const fs = require('fs-extra')
 const prompt = require('prompt')
+const moment = require('moment')
 
 prompt.message = ''
 prompt.start()
 
-prompt.get('title', function (err, res) {
+prompt.get('title', function(err, res) {
   console.log(`The post '${res.title}' has been created!`)
 
   // Make the directory
+  let date = moment().format('YYYY[-]MM[-]DD')
   let title = res.title.split(' ').join('-')
-  let dir = `./src/pages/blog/${title}`
+  let fileName = date + '---' + title
+  let dir = `./src/pages/blog/${fileName}`
   fs.ensureDir(dir)
 
   // Make the new empty post
-  let now = new Date()
+  let timeDate = new Date()
   let newPost = `---
 title: '${res.title}'
-date: '${now.toJSON()}'
+date: '${timeDate.toJSON()}'
 ---
   `
   let postPath = `${dir}/index.md`
   fs.writeFile(postPath, newPost, function(err, fileContents) {
-    if (err) throw err;
+    if (err) throw err
   })
 })
